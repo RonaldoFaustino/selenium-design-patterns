39af856eb8f4bbe821df44befa36655047ce3427
RonaldoFaustino <ohnirucse@msn.com>
2023-03-27 20:16:00 -0300

    Implementando componente result

---
+++ src/main/java/com/seleniumdesign/srp/ResultStat.java
@@ -0,0 +1,30 @@
+package com.seleniumdesign.srp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ResultStat extends AbstractComponent{
+
+    @FindBy(id = "result-stats")
+    private WebElement resultStatus;
+
+    public ResultStat(WebDriver driver) {
+        super(driver);
+    }
+
+    public String  getStatus(){
+        return this.resultStatus.getText();
+    }
+
+    @Override
+    public boolean isDisplayed() {
+        return this.wait.until((d) -> this.resultStatus.isDisplayed());
+    }
+
+    @Override
+    public boolean isDisplayed(WebElement webElement) {
+        return false;
+    }
+
+}
