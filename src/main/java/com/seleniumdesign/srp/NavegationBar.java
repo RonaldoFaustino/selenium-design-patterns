6bb19f99259ff9d0f5a5abbb34535d2c1a5442ce
RonaldoFaustino <ohnirucse@msn.com>
2023-03-27 19:58:49 -0300

    Implementando componente bar navegation

---
+++ src/main/java/com/seleniumdesign/srp/NavegationBar.java
@@ -0,0 +1,41 @@
+package com.seleniumdesign.srp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class NavegationBar extends AbstractComponent{
+
+    @FindBy(id = "top_nav")
+    private WebElement bar;
+
+    @FindBy(linkText = "Imagens")
+    private WebElement imagens;
+
+    @FindBy(linkText = "Noticias")
+    private WebElement noticias;
+
+    public NavegationBar(WebDriver driver) {
+        super(driver);
+    }
+
+    public void goToImagens(){
+        this.imagens.click();
+    }
+
+    public void goToNews(){
+        this.noticias.click();
+    }
+
+    @Override
+    public boolean isDisplayed() {
+        return this.wait.until((d) -> this.bar.isDisplayed());
+    }
+
+    @Override
+    public boolean isDisplayed(WebElement webElement) {
+        return false;
+    }
+
+
+}
