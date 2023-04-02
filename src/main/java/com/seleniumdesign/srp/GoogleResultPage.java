ed2b5373f4798cbcef788a994352ef783c75b7c7
RonaldoFaustino <ohnirucse@msn.com>
2023-04-02 16:28:51 -0300

    Implementando ResultPage

---
+++ src/main/java/com/seleniumdesign/srp/GoogleResultPage.java
@@ -0,0 +1,35 @@
+package com.seleniumdesign.srp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class GoogleResultPage {
+
+    private SearchWidget searchWidget;
+    private SearchSuggestion searchSuggestion;
+    private NavegationBar navegationBar;
+    private ResultStat  resultStat;
+
+    public GoogleResultPage (final WebDriver driver){
+        this.searchWidget = new SearchWidget(driver);
+        this.searchSuggestion = PageFactory.initElements(driver, SearchSuggestion.class);
+        this.navegationBar = PageFactory.initElements(driver, NavegationBar.class);
+        this.resultStat = PageFactory.initElements(driver, ResultStat.class);
+    }
+
+    SearchWidget getSearchWidget(){
+        return searchWidget;
+    }
+
+    SearchSuggestion getSearchSuggestion(){
+        return searchSuggestion;
+    }
+
+    NavegationBar getNavegationBar(){
+        return getNavegationBar();
+    }
+
+    ResultStat getResultStat(){
+        return resultStat;
+    }
+}
